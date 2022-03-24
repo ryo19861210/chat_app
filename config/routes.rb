b4e8c29aae7cd5680b471e205b9829cb705ff89b
ryo19861210 <superdio3633hornetkkauemi1210@gmail.com>
2022-03-24 18:40:51 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :new]
+  resources :rooms, only: [:create, :new]
 end
